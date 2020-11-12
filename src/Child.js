b9bad11541798632258eadd780ff50c9ade45a6f
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-12 16:49:44 +0500

    Updated Child.js and App.css
    modified Ui of App by giving styles in App.css

--- src/Child.js
+++ src/Child.js
@@ -2,7 +2,35 @@ import React from 'react';
 
 function Child(){
     return(
-        <div></div>
+        <div className="container">
+            <h1 className="text-center">Expense Tracker</h1>
+
+            <h3>Your Balance <br/> $260 </h3>
+
+            <div className="expense-container">
+                <h3>INCOME <br/> $500 </h3>
+                <h3>EXPENSE <br/> $240 </h3>
+            </div>
+            <h3>History</h3>
+            <hr/>
+
+            <h3>Add new transaction</h3>
+            <hr/>
+
+            <form className="transaction-form">
+                <label>
+                    Enter Description <br/>
+                    <input type="text" placeholder="Enter Description" required/>
+                </label>
+                <br/>
+                <label>
+                    Enter Amount <br/>
+                    <input type="number" placeholder="Enter Amount" required/>
+                </label>
+                <br/>
+                <input type="submit" value="ADD TRANSACTION"/>
+            </form>
+        </div>
     );
 }
 
