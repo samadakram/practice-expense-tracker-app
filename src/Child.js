3616930356b69952b9992c2e6e135227e43b2f7c
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-13 17:32:11 +0500

    Update Child.js

--- src/Child.js
+++ src/Child.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 function Child() {
+
+    let transactions = [
+        { amount: 500, desc: "Cash" },
+        { amount: 100, desc: "Cold Drink" },
+        { amount: 300, desc: "Hair" }
+    ]
+
     return (
         <div className="container">
             <h1 className="text-center">Expense Tracker</h1>
@@ -14,10 +21,14 @@ function Child() {
             <h3>History</h3>
             <hr />
             <ul className="transaction-list">
-                <li>
-                    <span> Cash </span>
-                    <span> $500 </span>
-                </li>
+                {transactions.map((transObj, ind) => {
+                    return (
+                        <li key={ind} >
+                            <span> ${transObj.desc} </span>
+                            <span> ${transObj.amount} </span>
+                        </li>
+                    )
+                })}
             </ul>
             <h3>Add new transaction</h3>
             <hr />
@@ -25,12 +36,12 @@ function Child() {
             <form className="transaction-form">
                 <label>
                     Enter Description <br />
-                    <input type="text" placeholder="Enter Description"/>
+                    <input type="text" placeholder="Enter Description" />
                 </label>
                 <br />
                 <label>
                     Enter Amount <br />
-                    <input type="number" placeholder="Enter Amount"/>
+                    <input type="number" placeholder="Enter Amount" />
                 </label>
                 <br />
                 <input type="submit" value="ADD TRANSACTION" />
