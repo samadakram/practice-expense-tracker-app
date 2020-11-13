c184bc901b87e1849480a8bcd0c3bd63ee1be506
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2020-11-13 21:57:17 +0500

    Update Child.js

--- src/Child.js
+++ src/Child.js
@@ -15,15 +15,35 @@ function Child() {
         })
     }
 
+    const getIncome = ()=>{
+        let income = 0;
+        for( var i = 0; i < transactions.length; i++){
+            if(transactions[i].amount > 0){
+                income += transactions[i].amount
+            }
+        }
+        return income;
+    }
+
+    const getExpense = ()=>{
+        let expense = 0;
+        for( var i = 0; i < transactions.length; i++){
+            if(transactions[i].amount < 0){
+                expense += transactions[i].amount
+            }
+        }
+        return expense;
+    }
+
     return (
         <div className="container">
             <h1 className="text-center">Expense Tracker</h1>
 
-            <h3>Your Balance <br /> %250 </h3>
+            <h3>Your Balance <br /> ${getIncome() + getExpense()} </h3>
 
             <div className="expense-container">
-                <h3>INCOME <br /> $400 </h3>
-                <h3>EXPENSE <br /> $200 </h3>
+                <h3>INCOME <br /> {getIncome()} </h3>
+                <h3>EXPENSE <br /> {getExpense()} </h3>
             </div>
             <h3>History</h3>
             <hr />
